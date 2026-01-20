b1325ed1643c4a5dcdcfbfca5176fad00f7a6663
HilbertYang <hilbert_yang@outlook.com>
2026-01-20 11:42:20 -0800

    add notation in DUT

--- fifo_day1/fifo.sv
+++ fifo_day1/fifo.sv
@@ -1,6 +1,6 @@
 module fifo_sync #(
   parameter int WIDTH = 8,
-  parameter int DEPTH = 16  // 建议是2的幂：8/16/32...
+  parameter int DEPTH = 16  // suggest to be the power of 2 like：8/16/32...
 ) (
   input  logic             clk,
   input  logic             rst_n,
@@ -21,16 +21,16 @@ module fifo_sync #(
   logic [WIDTH-1:0] mem [DEPTH];
   logic [ADDR_W-1:0] wptr, rptr;
 
-  // full/empty 基于 count（最直观，Day1够用）
+  //full and empty flags
   assign empty = (count == 0);
   assign full  = (count == DEPTH);
 
-  // 写/读握手（当不满/不空时才真正执行）
+  // control
   logic do_write, do_read;
   assign do_write = wr_en && !full;
-  assign do_read  = rd_en && !empty;
+  assign do_read  = rd_en && !empty;//&& is the logical AND operator. However the & is the bitwise AND operator.
 
-  // 读数据：同步读（读取发生在时钟沿）
+  // read data
   always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
       rdata <= '0;
@@ -39,25 +39,25 @@ module fifo_sync #(
     end
   end
 
-  // 指针 & 存储 & 计数
+  // Wire pointers and count
   always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
       wptr  <= '0;
       rptr  <= '0;
       count <= '0;
     end else begin
-      // 写
+      // write
       if (do_write) begin
         mem[wptr] <= wdata;
-        wptr <= (wptr == DEPTH-1) ? '0 : (wptr + 1);
+        wptr <= (wptr == DEPTH-1) ? '0 : (wptr + 1);//cyclic increment
       end
 
-      // 读
+      // read
       if (do_read) begin
         rptr <= (rptr == DEPTH-1) ? '0 : (rptr + 1);
       end
 
-      // count更新（同时读写则count不变）
+      // update count
       unique case ({do_write, do_read})
         2'b10: count <= count + 1;
         2'b01: count <= count - 1;
